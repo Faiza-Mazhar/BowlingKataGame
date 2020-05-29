a462b4b15a3f906295fde7adce84b09dcd77b3af
Faiza Mazhar <faiza.mazhar@bbc.co.uk>
2020-05-29 14:51:30 +0100

    Bowling Kata with Game Class ans Tests

--- app/src/main/java/com/example/bowlingkatagame/Game.kt
+++ app/src/main/java/com/example/bowlingkatagame/Game.kt
@@ -2,14 +2,53 @@ package com.example.bowlingkatagame
 
 class Game {
 
-    private var score = 0
+    private val rollScore: IntArray = IntArray(21)
+    private var currentRoll = 0
+
+    private val _BEST_SCORE = 10
+    private val _FRAME_SIZE = 10
 
     fun rollBall(pins: Int) {
-        score += pins
+        rollScore[ currentRoll++ ] = pins
     }
 
     fun getScore() :Int {
+        var score = 0
+        var frameIndex = 0
+        for(index in 0 until _FRAME_SIZE){
+
+            if(isSpare(frameIndex)){ //spare
+                score += spareBonus(frameIndex) + _BEST_SCORE
+                frameIndex += 2
+            } else if(isStrike(frameIndex)) {
+                score += getStrikeBonus(frameIndex) + _BEST_SCORE
+                frameIndex += 1
+            }else {
+                score += getFrameScore(frameIndex)
+                frameIndex += 2
+            }
+        }
         return score
+    }
+
+    private fun isSpare(frameIndex : Int) :Boolean {
+        return rollScore[frameIndex] + rollScore[frameIndex + 1] == _BEST_SCORE
+    }
+
+    private fun spareBonus(frameIndex: Int): Int {
+        return rollScore[frameIndex + 2]
+    }
+
+    private fun getFrameScore(frameIndex: Int) :Int {
+        return rollScore[frameIndex] + rollScore[frameIndex+1]
+    }
+
+    private fun isStrike(frameIndex: Int) :Boolean {
+        return rollScore[frameIndex] == _BEST_SCORE
+    }
+
+    private fun getStrikeBonus(frameIndex: Int) : Int {
+        return getFrameScore(frameIndex + 1)
     }
 
 }
--- app/src/test/java/com/example/bowlingkatagame/GameTest.kt
+++ app/src/test/java/com/example/bowlingkatagame/GameTest.kt
@@ -1,6 +1,7 @@
 package com.example.bowlingkatagame
 
 import org.junit.Assert
+import org.junit.Before
 import org.junit.Test
 
 /**
@@ -9,23 +10,81 @@ import org.junit.Test
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 class GameTest {
+
+    private lateinit var game: Game
+
+    @Before
+    fun setUp(){
+        game = Game()
+    }
+
     @Test
     fun `Test gutter game`(){
-        val game = Game()
-
-        for(index in 0 until 20){
-            game.rollBall(0)
-        }
+        rollMultiplesBall(20, 0)
+        Assert.assertEquals(0, 0)
     }
 
     @Test
     fun `getScore return the score`() {
-        val game = Game()
-
-        for(index in 0 until 20){
-            game.rollBall(1)
-        }
-
+        rollMultiplesBall(20, 1)
         Assert.assertEquals(20, game.getScore())
+    }
+
+    @Test
+    fun `test that spare is counted`() {
+        rollSpare()
+        game.rollBall(4)
+
+        rollMultiplesBall(17, 0)
+
+        Assert.assertEquals(18, game.getScore())
+    }
+
+    @Test
+    fun `test that strike is counted`() {
+
+        rollStrike()
+        game.rollBall(6)
+        game.rollBall(2)
+
+        rollMultiplesBall(17, 0)
+        Assert.assertEquals(26, game.getScore())
+    }
+
+    @Test
+    fun `test perfect game`() {
+        rollMultiplesBall(20, 10)
+        Assert.assertEquals(300, game.getScore())
+    }
+
+    @Test
+    fun `test strike in last frame`() {
+        rollMultiplesBall(18, 1)
+        rollStrike()
+        game.rollBall(6)
+        Assert.assertEquals(34, game.getScore())
+    }
+
+    @Test
+    fun `test spare in last frame`() {
+        rollMultiplesBall(18, 1)
+        rollSpare()
+        game.rollBall(6)
+        Assert.assertEquals(34, game.getScore())
+    }
+
+    private fun rollMultiplesBall(times: Int, pins: Int ) {
+        for( index in 0 until times) {
+            game.rollBall(pins)
+        }
+    }
+
+    private fun rollSpare() {
+        game.rollBall(7)
+        game.rollBall(3)
+    }
+
+    private fun rollStrike() {
+        game.rollBall(10)
     }
 }
